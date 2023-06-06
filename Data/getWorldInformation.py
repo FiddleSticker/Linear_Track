d233e5725362da30464544500688396cbda24df0
William Forchap <waforchap@aol.com>
2023-06-06 14:30:09 +0200

    Fixed getWorldInformation.py bug

--- Data/getWorldInformation.py
+++ Data/getWorldInformation.py
@@ -13,11 +13,11 @@ from cobel.observations.image_observations import ImageObservationBaseline
 os.environ['BLENDER_EXECUTABLE_PATH'] = "C:/Users/wafor/Desktop/blender-2.79b-windows64/blender.exe"
 
 if __name__ == '__main__':
-    demo_scene = "C:/Users/wafor/Desktop/Transfer/cobel_saves/environments_blender/linear_track_1x4.blend"
+    demo_scene = "C:/Users/wafor/Desktop/cobel_saves/environments_blender/linear_track_1x4.blend"
+    # demo_scene = "C:/Users/wafor/Desktop/Bachelorarbeit/Cobel/CoBeL-RL/cobel/environments/environments_blender/simple_grid_graph_maze.blend"
 
     path = os.path.dirname(os.path.abspath(__file__))
-    info_path = os.path.split(demo_scene)[-1].split(".")[0]
-    info_path = os.path.join(path, info_path)
+    output_path = "C:/Users/wafor/Desktop/linear_track_4"
 
     mainWindow = qg.GraphicsLayoutWidget(title='Demo: DQN')
     mainWindow.show()
@@ -26,29 +26,29 @@ if __name__ == '__main__':
     modules = dict()
     modules['world'] = FrontendBlenderInterface(demo_scene)
     modules['observation'] = ImageObservationBaseline(modules['world'], mainWindow, True)
-    modules['spatial_representation'] = ManualTopologyGraphNoRotation(modules, {'startNodes': [0], 'goalNodes': [3],
-                                                                                'cliqueSize': 2})
+    modules['spatial_representation'] = ManualTopologyGraphNoRotation(modules, {'start_nodes': [0], 'goal_nodes': [3],
+                                                                                'clique_size': 2})
     modules['spatial_representation'].set_visual_debugging(True, mainWindow)
 
     # delete previous directory contents
     # os.system('rm -rf ' + info_path)
     # re-create the worldInfo directory
-    os.makedirs(info_path, exist_ok=True)
+    os.makedirs(output_path, exist_ok=True)
 
-    safeZoneDimensions = np.array([modules['world'].minX, modules['world'].minY, modules['world'].minZ,
-                                   modules['world'].maxX, modules['world'].maxY, modules['world'].maxZ])
-    np.save(info_path + '/safeZoneDimensions.npy', safeZoneDimensions)
-    np.save(info_path + '/safeZonePolygon.npy', modules['world'].safeZonePolygon)
-    np.save(info_path + '/safeZoneVertices.npy', modules['world'].safeZoneVertices)
-    np.save(info_path + '/safeZoneSegments.npy', modules['world'].safeZoneSegments)
+    safeZoneDimensions = np.array([modules['world'].min_x, modules['world'].min_y, modules['world'].min_z,
+                                   modules['world'].max_x, modules['world'].max_y, modules['world'].max_z])
+    np.save(output_path + '/safeZoneDimensions.npy', safeZoneDimensions)
+    np.save(output_path + '/safeZonePolygon.npy', modules['world'].safe_zone_polygon)
+    np.save(output_path + '/safeZoneVertices.npy', modules['world'].safe_zone_vertices)
+    np.save(output_path + '/safeZoneSegments.npy', modules['world'].safe_zone_segments)
 
     # store environment information
-    nodes = np.array(modules['world'].getManuallyDefinedTopologyNodes())
+    nodes = np.array(modules['world'].get_manually_defined_topology_nodes())
     nodes = nodes[nodes[:, 0].argsort()]
-    edges = np.array(modules['world'].getManuallyDefinedTopologyEdges())
+    edges = np.array(modules['world'].get_manually_defined_topology_edges())
     edges = edges[edges[:, 0].argsort()]
-    np.save(info_path + '/topologyNodes.npy', nodes)
-    np.save(info_path + '/topologyEdges.npy', edges)
+    np.save(output_path + '/topologyNodes.npy', nodes)
+    np.save(output_path + '/topologyEdges.npy', edges)
 
     # store referenceImages sampled images from contexts A and B
 
@@ -57,8 +57,8 @@ if __name__ == '__main__':
     # prepare context A
     # switch on white light
 
-    imageDims = (30, 1)
-    referenceImages = []
+    image_dims = (30, 1)
+    reference_images = []
 
     for ni in range(len(modules['spatial_representation'].nodes)):
         node = modules['spatial_representation'].nodes[ni]
@@ -66,22 +66,22 @@ if __name__ == '__main__':
         if node.index != -1:
             # propel the simulation
             modules[
-                'spatial_representation'].nextNode = node.index  # required by the WORLD_ABAFromImagesInterface
+                'spatial_representation'].next_node = node.index  # required by the WORLD_ABAFromImagesInterface
             modules['world'].step_simulation_without_physics(node.x, node.y, 90.0)
             # the observation is plainly the robot's camera image data
-            observation = modules['world'].envData['imageData']
+            observation = modules['world'].env_data['image']
             # for now, cut out a single line from the center of the image (w.r.t. height) and use this as an observation in order
             # to save computational resources
             # observation=observation[29:30,:,:]
             # scale the one-line image to further reduce computational demands
-            observation = cv2.resize(observation, dsize=(imageDims))
+            observation = cv2.resize(observation, dsize=(image_dims))
             # observation=np.flip(observation,0)
             # cv2.imshow('Test',observation)
             # cv2.waitKey(0)
-            referenceImages += [observation]
+            reference_images += [observation]
 
-    images = np.array(referenceImages)
-    np.save(info_path + '/images.npy', images)
-    modules['world'].stopBlender()
+    images = np.array(reference_images)
+    np.save(output_path + '/images.npy', images)
+    modules['world'].stop_blender()
     # and also stop visualization
     mainWindow.close()
